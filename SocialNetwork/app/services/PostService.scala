72ebe588904e74061cd02aa9e497ed4c14dba4cb
markoceran <markoceran88mc@gmail.com>
2024-03-28 11:25:04 +0100

    validatePostContent

--- SocialNetwork/app/services/PostService.scala
+++ SocialNetwork/app/services/PostService.scala
@@ -9,8 +9,8 @@ import scala.concurrent.{ExecutionContext, Future}
 class PostService @Inject()(postRepository: PostRepository) (implicit ec: ExecutionContext){
 
   def createPost(postedById: BigInt, newPost: NewPost): Future[(Boolean, String)] = {
-    if (newPost.content.length <= 250 && newPost.content != "" && newPost.content.length > 0) {
-      postRepository.createPost(postedById, newPost.content).map(created => (created, ""))
+    if (validatePostContent(newPost.content)) {
+      postRepository.createPost(postedById, newPost.content).map(createdSuccess => (createdSuccess, ""))
     } else {
       Future.successful(false, "Post must contain more than 0 characters and no more that 250 characters")
     }
@@ -27,8 +27,8 @@ class PostService @Inject()(postRepository: PostRepository) (implicit ec: Execut
     postRepository.getPostById(updatePost.postId).flatMap {
       case Some(post) =>
         if(post.postedBy.id == userId){
-          if (updatePost.newContent.length <= 250 && updatePost.newContent != "" && updatePost.newContent.length > 0) {
-            postRepository.editPost(updatePost.postId, updatePost.newContent).map(created => (created, ""))
+          if (validatePostContent(updatePost.newContent)) {
+            postRepository.editPost(updatePost.postId, updatePost.newContent).map(editedSuccess => (editedSuccess, ""))
           } else {
             Future.successful(false, "Post must contain more than 0 characters and no more that 250 characters")
           }
@@ -39,5 +39,12 @@ class PostService @Inject()(postRepository: PostRepository) (implicit ec: Execut
     }
   }
 
+  private def validatePostContent(content: String): Boolean = {
+    if (content.length <= 250 && content != "" && content.length > 0) {
+      true
+    } else {
+      false
+    }
+  }
 
 }
